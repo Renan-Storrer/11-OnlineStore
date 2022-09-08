c769af9701ec0b53325a2ceecb9d497745e7a861
RoberthDS <roberthgt158@gmail.com>
2022-09-08 18:56:59 -0300

    feito requisito 03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Home } />
+        <Route exact path="/" component={ Home } />
+        <Route path="/shoppingcart" component={ ShoppingCart } />
       </BrowserRouter>
     );
   }
