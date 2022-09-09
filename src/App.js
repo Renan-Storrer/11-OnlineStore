330b07faef7c39d0faa78b79344bd5086833f4d3
Leonardo Leite <leoinatel@gmail.com>
2022-09-09 19:32:10 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import propTypes from 'prop-types';
 import Home from './components/Home';
+import ProductDetail from './components/ProductDetail';
 import ShoppingCart from './components/ShoppingCart';
 
 class App extends React.Component {
   render() {
+    const { id } = this.props;
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Home } />
-        <Route path="/shoppingcart" component={ ShoppingCart } />
+        <Route exact path="/shoppingcart" component={ ShoppingCart } />
+        <Route
+          exact
+          path="/productdetail/:id"
+          render={ (props) => <ProductDetail { ...props } id={ id } /> }
+        />
       </BrowserRouter>
     );
   }
 }
 
 export default App;
+
+App.propTypes = {
+  id: propTypes.string,
+}.isRequired;
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,26 +1,58 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { getProductById } from '../services/api';
 
 class ProductDetail extends React.Component {
-  handleClick() {
-    <Link to="/shoppingcart" />;
+  state = {
+    product: [],
+  };
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.callGetProductById(id);
+  }
+
+  async callGetProductById(id) {
+    const product = await getProductById(id);
+    this.setState({ product });
   }
 
   render() {
+    const { product } = this.state;
     return (
       <section className="productDetail">
-        <img src="" alt="" />
-        <p>Nome:</p>
-        <p>Especificação Técnica:</p>
-        <p>Preço:</p>
-        <button
-          type="button"
-          onClick={ this.handleClick }
+        {console.log(product)}
+        <img
+          src={ product.thumbnail }
+          alt={ product.title }
+          data-testid="product-detail-image"
+        />
+        <p data-testid="product-detail-name">
+          Nome:
+          {product.title}
+        </p>
+        <p>{product.warranty}</p>
+        <p data-testid="product-detail-price">
+          Preço:
+          {product.price}
+        </p>
+        <Link
+          to="/shoppingCart"
         >
-          Ir para o Carrinho
-        </button>
+          <button
+            data-testid="shopping-cart-button"
+            type="button"
+          >
+            Ir para o carrinho
+          </button>
+        </Link>
       </section>
     );
   }
 }
 export default ProductDetail;
+
+ProductDetail.propTypes = {
+  id: propTypes.string,
+}.isRequired;
